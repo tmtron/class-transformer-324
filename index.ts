4a756b764d0624c141ed666a68e46d1f3d64200f
TmTron <martin.trummer@tmtron.com>
2020-01-29 11:02:58 +0100

    improved comments

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import "reflect-metadata";
 import {plainToClass} from "class-transformer";
 import {IsString} from 'class-validator'
 
+console.log('test project for: https://github.com/typestack/class-transformer/issues/324');
+
 class MyPayload {
     @IsString()
     prop: string;
@@ -16,6 +18,6 @@ const result2 = plainToClass(MyPayload, { prop: 1234 }, { enableImplicitConversi
  *  result2 will be `{ prop: 1234 }` - default behaviour
  */
 
-console.log(result1) // MyPayload { prop: 1234 }
-console.log(typeof result1.prop) // MyPayload { prop: 1234 }
-console.log(result2) // MyPayload { prop: 1234 }
+console.log(result1); // MyPayload { prop: '1234' }
+console.log(typeof result1.prop); // string
+console.log(result2); // MyPayload { prop: 1234 }
